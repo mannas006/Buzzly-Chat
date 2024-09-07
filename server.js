f6166634a3ae82aa547a8dab4e3c617c5cb49bcd
Manas Dey <manasdey.iron006@gmail.com>
2024-09-08 01:03:11 +0530

    fix server

--- server.js
+++ server.js
@@ -1,90 +1,44 @@
-const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const express = require('express');
+const { Server } = require('socket.io');
 
+// Create an Express application
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
+// Create a new instance of Socket.IO
+const io = new Server(server);
+
+// Middleware to serve static files
 app.use(express.static('public'));
 
-let users = [];
-let userNames = {};
-
+// Socket.IO connection handler
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
     socket.on('setName', (name) => {
-        userNames[socket.id] = name;
-
-        if (users.length > 0) {
-            const partner = users.pop();
-            const partnerName = userNames[partner];
-            socket.emit('connected', partnerName);
-            io.to(partner).emit('connected', userNames[socket.id]);
-
-            // Forward messages between users
-            socket.on('sendMessage', (message) => {
-                io.to(partner).emit('receiveMessage', `${userNames[socket.id]}: ${message}`);
-                socket.emit('receiveMessage', `You: ${message}`);
-            });
-
-            socket.on('skip', () => {
-                socket.emit('receiveMessage', 'You have skipped. Wait for another user.');
-                io.to(partner).emit('receiveMessage', 'Your chat partner has skipped. You are now waiting for a new user.');
-                users.push(partner);
-                users.push(socket.id);
-            });
-
-        } else {
-            users.push(socket.id);
-            socket.emit('waiting');
-        }
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-        users = users.filter(user => user !== socket.id);
-        delete userNames[socket.id];
-    });
-});
-
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
-
-
-// api/chat.js (or any name inside 'api' folder)
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const io = new Server(createServer());
-
-io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
-    socket.on('setName', (name) => {
-        // Your existing logic
+        socket.name = name;
+        // Notify the user that they are connected
+        socket.emit('connected', name);
+        // Broadcast that a new user has joined
+        socket.broadcast.emit('waiting');
     });
 
     socket.on('sendMessage', (message) => {
-        // Your existing logic
+        io.emit('receiveMessage', `${socket.name}: ${message}`);
     });
 
     socket.on('skip', () => {
-        // Your existing logic
+        io.emit('waiting');
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
-        // Your existing logic
     });
 });
 
-module.exports = (req, res) => {
-    if (req.method === 'GET') {
-        // Handle GET requests
-        res.status(200).send('Socket.IO server is running');
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
-};
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
